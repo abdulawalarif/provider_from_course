24356f7401d146fc74d04f0198aca97159af368d
abdul awal <awal@twintechsoft.com>
2024-11-21 04:30:49 +0600

    added BreadCrumb class

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello world'),
-        ),
-      )
-    );
+    return const MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        home: const Scaffold(
+          body: Center(
+            child: Text('Hello world'),
+          ),
+        ));
   }
 }
